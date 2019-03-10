7f460e9b4ddfc0ae3635c7359dab36b192d9fa02
fh9501 <1119369173@qq.com>
2019-03-10 22:14:36 +0800

    Import assets

---
+++ Assets/Scripts/Animation/AnimatedCookie.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Animation
+{
+    public class AnimatedCookie : MonoBehaviour
+    {
+        [SerializeField] private Texture[] cookies;
+        [SerializeField] private float framesPerSecond = 15;
+        private Light _light;
+        private int _index = 0;
+        private float _nextCookieTime = 0;
+
+        private void Awake()
+        {
+            _light = GetComponent<Light>();
+        }
+
+        private void OnEnable()
+        {
+            _nextCookieTime = Time.time;
+        }
+
+        private void Update()
+        {
+            float time = Time.time;
+            if (time >= _nextCookieTime)
+            {
+                _light.cookie = cookies[_index];
+                ++_index;
+                if (_index == cookies.Length)
+                {
+                    _index = 0;
+                }
+
+                float step = 1 / framesPerSecond;
+                _nextCookieTime += step;
+                if (_nextCookieTime <= time)
+                {
+                    _nextCookieTime = time + step;
+                }
+            }
+        }
+    }
+}
