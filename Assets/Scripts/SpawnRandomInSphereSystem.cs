db3df86e6c893003bd9bf97e21685b03c243bc6d
fh9501 <1119369173@qq.com>
2019-03-15 17:38:55 +0800

    SpawnRandomInSphereSystem

---
+++ Assets/Scripts/SpawnRandomInSphereSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+namespace DefaultNamespace
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateAfter(typeof(TransformSystemGroup))]
+    public class SpawnRandomInSphereSystem : ComponentSystem
+    {
+        private ComponentGroup _mainGroup;
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+            _mainGroup = GetComponentGroup(ComponentType.ReadOnly<SpawnRandomInSphere>(),
+                ComponentType.ReadOnly<LocalToWorld>());
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
